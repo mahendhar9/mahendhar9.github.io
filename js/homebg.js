02af65e2c25243177b530c7202b021c958390134
Mahendhar <mahendarreddy@live.com>
2016-01-03 03:05:06 +0530

    Added voice recognition

--- js/homebg.js
+++ js/homebg.js
@@ -1,25 +1,46 @@
 $(document).ready(function(){
 	$("#home").particleground({
 		dotColor: '#ecf0f1',
-        lineColor: '#263142',
-        density:12000
-                    
+		lineColor: '#263142',
+		density:12000
+
 	});
 
+	$('#voiceModal').modal('show');
+
+	if (annyang) {
+		var commands = {
+			'go to *term': function(term) {
+				console.log('Go to ' + term.toString());
+				$('#voice-' + term.toString() + ' a').click();
+			},
+
+      ':projectCategory projects': function(projectCategory) {
+      	console.log(projectCategory);
+        if(projectCategory.toString() == "all") {
+          console.log("See " + projectCategory.toString() + ' projects')
+          $("#all9").click();
+        } else if(projectCategory.toString().toLowerCase() == "html") {
+        	 console.log("See " + projectCategory.toString() + ' projects')
+          $("#html9").click();
+        } else if(projectCategory.toString().toLowerCase() == "javascript") {
+        	 console.log("See " + projectCategory.toString() + ' projects')
+          $("#javascript9").click();
+        } else if(projectCategory.toString().toLowerCase() == "rails") {
+        	 console.log("See " + projectCategory.toString() + ' projects')
+          $("#rails9").click();
+        } else if(projectCategory.toString().toLowerCase() == "angular") {
+        	 console.log("See " + projectCategory.toString() + ' projects')
+          $("#angular9").click();
+        }
+      }
+		};
+		annyang.addCommands(commands);
+
+		annyang.start();
+	}
+
 	new WOW().init();	
-
-	// $("#all9").click(function(ev){
-	// 	ev.preventDefault();
-	// 	$("#calci1").fadeIn(1500);
-	// 	$("#psd1").fadeIn(1500);
-	// });
-
-	// $("#html").click(function(ev){
-	// 	ev.preventDefault();
-
-	// 	// $("#calci1").fadeOut(1500);
-	// 	// $("#psd1").fadeIn(1500);
-	// });
 
 	$("#all9").click(function(ev){
 		ev.preventDefault();		
@@ -63,69 +84,32 @@ $(document).ready(function(){
 	});
 
 
-
-	// $("#calci9").click(function(ev){
-	// 	ev.preventDefault();
-	// 	$("#psd1").fadeOut(1500);
-	// 	$("#calci1").fadeIn(1500);
-	// });
+	$('[data-toggle="tooltip"]').tooltip(); 
 
 
-    $('[data-toggle="tooltip"]').tooltip(); 
-
-    
-  $('nav a[href*=#], #home a[href*=#]').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-    && location.hostname == this.hostname) {
-      var $target = $(this.hash);
-      $target = $target.length && $target
-      || $('[name=' + this.hash.slice(1) +']');
-      if ($target.length) {
-        var targetOffset = $target.offset().top;
-        $('html,body')
-        .animate({scrollTop: targetOffset}, 1000);
-       return false;
-      }
-    }
+	  $('nav a[href*=#], #home a[href*=#]').click(function() {
+		    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+			    && location.hostname == this.hostname) {
+			      var $target = $(this.hash);
+		      $target = $target.length && $target
+		      || $('[name=' + this.hash.slice(1) +']');
+		      if ($target.length) {
+			        var targetOffset = $target.offset().top;
+			        $('html,body')
+			        .animate({scrollTop: targetOffset}, 1000);
+			       return false;
+		      }
+	    }
   });
 
 	$('body').scrollspy({ target: 'nav' });
 
-  $('nav').affix({
-	  offset: {
-	    top: 100
-	  }
+	$('nav').affix({
+		offset: {
+			top: 100
+		}
 	})
 
 
-	// $("#sun").click(function(){
-	// 	$("#contact-me").css("background-image", "url('../img/bg9.jpg')").fadeIn();
-	// 	$("#contact-me h2").css("color","#fffff");
-	// 	$("#contact-me p strong").css("color","#fffff");
-
-	// });
-
-
-	// function car(){
-	// 	$(".beetle").css({left:'-128px'});
-	//     $(".beetle").animate({left: "+=100%"}, 15000);
-	// }
-
-	// car();
-
-	// function moveBackCar() {
-	// 	$(".beetle").parent().css(position: 'relative');
-	// 	$(".beetle").css({position:'absolute', left:0%, top:-12%});
-	// }
-
-	// moveBackCar();
-
-	// $.when(car()).done(function() {
- 	//    moveBackCar();
-	// });
-
-
-	
-	// setInterval(function(){car()}, 5000);
 
 });
